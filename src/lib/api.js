5da5ad597ce94861c306240a5d2e60e88333439a
Quin Carlson <quintuscarlson@gmail.com>
2026-04-04 20:56:50 -0700

    prepare for vercel deployment

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3001";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
 export async function startInterview(setup) {
   const res = await fetch(`${API_BASE}/api/interview/start`, {
